bd22d0111cc0a1af8082328b68449d1c0d576e28
venturads <albert@venturads.com>
2020-07-01 23:38:33 -0700

    add router routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
       <h2>hello world!</h2>
-    </div>
+      <Route path='/' />
+      <Route path='/contact' component={Contact} />
+      <Route path='/about' component={About} />
+    </Router>
   );
 }
 
