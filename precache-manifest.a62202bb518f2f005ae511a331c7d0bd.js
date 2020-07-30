46994472a7bc11c36405f60bf5b87dbd4bac195e
venturads <albert@venturads.com>
2020-07-30 15:54:33 -0700

    Updates

---
+++ precache-manifest.a62202bb518f2f005ae511a331c7d0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ec7b8a45406f2b9d699a7e0bbe3597",
+    "url": "/gignow/index.html"
+  },
+  {
+    "revision": "362a5701b4f26b313ea2",
+    "url": "/gignow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b89c8c72e87f3380398f",
+    "url": "/gignow/static/js/2.ef32530c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gignow/static/js/2.ef32530c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362a5701b4f26b313ea2",
+    "url": "/gignow/static/js/main.d9479e1f.chunk.js"
+  },
+  {
+    "revision": "8eed4d7a45855e7debbd",
+    "url": "/gignow/static/js/runtime-main.12c46d5c.js"
+  }
+]);
